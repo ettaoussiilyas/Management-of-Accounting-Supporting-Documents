9b49a4a9dc806f8c7866ffe2e80c57ea404f72f1
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-11-17 19:24:58 +0100

    Setup Project and his dependencies based on context requirements

---
+++ src/main/java/com/cabinetcomptable/security/CabinetComptableAlAmaneApplication.java
@@ -0,0 +1,13 @@
+package com.cabinetcomptable.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetComptableAlAmaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetComptableAlAmaneApplication.class, args);
+    }
+
+}
